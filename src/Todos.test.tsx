ccc8c140997d7fa2f01f0c368e6f5b53fdf24ef7
David Jebing <david.jebing@lise.de>
2020-09-04 17:50:25 +0200

    add functionality to add todos

--- src/Todos.test.tsx
+++ src/Todos.test.tsx
@@ -1,4 +1,4 @@
-import { screen, render } from "@testing-library/react"
+import { screen, render, fireEvent } from "@testing-library/react"
 import React from 'react'
 import { Todos } from './Todos'
 
@@ -6,4 +6,26 @@ test('should show input for new todo', () => {
     render(<Todos />)
     const input = screen.getByPlaceholderText(/add something/i)
     expect(input).toBeVisible();
+})
+test('should add new todo', () => {
+    render(<Todos />)
+    const input = screen.getByPlaceholderText(/add something/i)
+    const addButton = screen.getByRole('button', { name: 'add' })
+    const todo = "Test Todo"
+    fireEvent.change(input, { target: { value: todo } })
+    fireEvent.click(addButton);
+
+    const todoElement = screen.getByText(todo);
+
+    expect(todoElement).toBeVisible();
+})
+test('should clear new todo after adding', () => {
+    render(<Todos />)
+    const input = screen.getByPlaceholderText(/add something/i)
+    const addButton = screen.getByRole('button', { name: 'add' })
+    const todo = "Test Todo"
+    fireEvent.change(input, { target: { value: todo } })
+    fireEvent.click(addButton);
+
+    expect(input).toHaveValue('');
 })
--- src/Todos.tsx
+++ src/Todos.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
+
+interface TodoItem {
+  text: string;
+}
 
 export function Todos() {
-    return <div>
-        <input placeholder="Add something"></input>
-    </div>;
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const addNewTodo = () => {
+    setTodos((current) => [...current, { text: newTodo }]);
+    setNewTodo("");
+  };
+
+  return (
+    <div>
+      <input
+        placeholder="Add something"
+        value={newTodo}
+        onChange={(event) => setNewTodo(event.target.value)}
+      ></input>
+      <button onClick={addNewTodo}>add</button>
+      <ul>
+        {todos.map((todo, i) => (
+          <li key={i}>{todo.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
