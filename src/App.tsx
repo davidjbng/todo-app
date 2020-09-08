c67fdb1cc92298d06a26ddfe26bd41199a27ae75
David Jebing <david.jebing@lise.de>
2020-09-08 11:41:45 +0200

    add base styles for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
-import { Todos } from './Todos';
+import React from "react";
+import "./App.css";
+import { Todos } from "./Todos";
 
 function App() {
   return (
-    <div>
+    <div className="app-container">
       <Todos></Todos>
     </div>
   );
