59a021c9c94f0c6d3c862bb67d25317b50e0b66f
David Jebing <david.jebing@lise.de>
2020-09-10 22:00:32 +0200

    add example todos

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,28 @@ import React from "react";
 import "./App.css";
 import { Todos } from "./Todos";
 
+const exampleTodos = [
+  {
+    id: 1,
+    title: "hello",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "click me",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "I'm completed",
+    completed: true,
+  },
+];
+
 function App() {
   return (
     <div className="app-container">
-      <Todos></Todos>
+      <Todos initialTodos={exampleTodos}></Todos>
     </div>
   );
 }
