22ba1cb360f88900093108ce028a18445552d993
David Jebing <david.jebing@lise.de>
2020-09-11 11:24:15 +0200

    add example for wait for disappearance

--- src/TodosWithServer.test.tsx
+++ src/TodosWithServer.test.tsx
@@ -1,11 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { rest } from "msw";
 import React from "react";
 import { TodosWithServer } from "./TodosWithServer";
 import { server } from "./__mocks__/server";
-
-beforeEach(() => {});
 
 test("should load todos", async () => {
   server.use(
@@ -18,10 +20,22 @@ test("should load todos", async () => {
 
   expect(await screen.findByText("test1")).toBeInTheDocument();
 });
+test("should load todos (example with wait for disappearance)", async () => {
+  server.use(
+    rest.get("/todos", (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json([{ title: "test1", id: 1 }]));
+    })
+  );
+  render(<TodosWithServer />);
+
+  await waitForElementToBeRemoved(screen.getByText("Loading..."));
+
+  expect(screen.getByText("test1")).toBeInTheDocument();
+});
 test("should add new todo with api", async () => {
   server.use(
     rest.post("/todos", (req, res, ctx) => {
-      return res(ctx.json({ title: "new todo", completed: false, id: 3 }));
+      return res(ctx.json({ ...JSON.parse(req.body as string), id: 1 }));
     })
   );
   render(<TodosWithServer />);
