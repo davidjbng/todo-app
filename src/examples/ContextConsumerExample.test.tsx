0769cefed7fa8a28eccb34572b1d80caf86ebe3f
David Jebing <david.jebing@lise.de>
2020-09-14 12:31:30 +0200

    remove msw and examples
    msw is not supported in codesandbox

--- src/examples/ContextConsumerExample.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "test-utils";
-import ContextConsumerExample from "./ContextConsumerExample";
-
-test("should render", () => {
-  render(<ContextConsumerExample></ContextConsumerExample>);
-
-  expect(screen.getByText("Hello, ThemeContext!")).toBeInTheDocument();
-});
